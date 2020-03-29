4c8c84e2bd42330da2cb918f244a6fc1ac69c1bf
Riley Kuhlman <rilstarssun@gmail.com>
2020-03-29 12:46:40 -0500

    added some basic styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
 import React from "react";
 import "./styles.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, NavLink, Link } from "react-router-dom";
 import EditPage from "./EditPage";
 import HomePage from "./HomePage";
 import GeneratePage from "./GeneratePage";
+
+import icon from "./img/creativity.svg"
 
 // have the backend be in localstorage
 // store a variable with the list of the names of lists
 // store a variable with the list of items for each list the user creates
 
 export default function App() {
+  const scrollTop = () => {
+    window.scrollTo(0, 0);
+  }
+  
+  let year = new Date();
+  year = year.getFullYear();
+
   return (
     <Router>
       {/*Navbar*/}
-      <Switch>
-        <Route path="/edit" component={EditPage} />
-        <Route path="/generate">
-          <GeneratePage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
+      <div className="navbar">
+        <Link to="/"className="icon-title">
+          <img src={icon} />
+          <h3>Idea Generator</h3>
+        </Link>
+        <div className="links">
+          <NavLink to="/" activeClassName="selected" exact>Home</NavLink>
+          <NavLink to="/edit" activeClassName="selected">Edit</NavLink>
+          <NavLink to="/generate" activeClassName="selected">Generate</NavLink>
+        </div>
+      </div>
+      <div className="main-content">
+        <Switch>
+          <Route path="/edit" component={EditPage} />
+          <Route path="/generate">
+            <GeneratePage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
       {/*Footer*/}
+      <div className="footer">
+        <div className="scaling-grid">
+          <div>
+            The main goal of the idea generator is to help you come up with ideas so that you can create something. Make your own list of options or just use some of the pregenerated ones and see what you can come up with.
+          </div>
+
+          <div>
+            <img src={icon} />
+          </div>
+          
+          <div>
+            <h2 className="footer-title">Idea Generator</h2>
+            <div className="links">
+              <a onClick={scrollTop}>Top</a>
+              <Link to="/">Home</Link>
+              <Link to="/edit">Edit</Link>
+              <Link to="/generate">Generate</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="copyright">
+          © {year} Riley Kuhlman 
+        </div>
+
+        {/* 
+        description of what the website is trying to do
+        icon
+        Copyright info (make the year update automatically)
+        scroll to top
+        links to edit page, home page, and generate
+        */}
+      </div>
     </Router>
   );
 }
--- src/HomePage.js
+++ src/HomePage.js
@@ -8,11 +8,13 @@ class HomePage extends Component {
         <h1>Idea Generator</h1>
 
         <p>
-          Struggling with creativity?
-          <br />
-          <Link to="/edit/newList">Create a new list</Link> or{" "}
+          Struggling with creativity? <Link to="/edit/newList">Create a new list</Link> or{" "}
           <Link to="/generate">use a pregenerated one</Link> to help you come up
           with ideas.
+        </p>
+
+        <p>
+          The principle behind this website is that there are so many good ideas and good combinations of ideas floating around out there, but we just haven't seen them in the right order yet. So, by allowing you to create lists of items and then insert a random item from that list into a sentence, mad libs style, I hope that you can come up with your next big idea.
         </p>
       </div>
     );
